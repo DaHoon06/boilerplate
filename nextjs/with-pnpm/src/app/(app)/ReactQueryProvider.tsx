d0a9de88343d1bb95ebacea2bbe0fef0ba30d3f3
dahoon06 <dahoon226@naver.com>
2025-03-14 23:54:16 +0900

    no message

--- nextjs/with-pnpm/src/app/(app)/ReactQueryProvider.tsx
+++ nextjs/with-pnpm/src/app/(app)/ReactQueryProvider.tsx
@@ -7,7 +7,7 @@ import {
   QueryClientProvider,
 } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { queryClient } from "@/shared/libs/tanstack/queryClient";
+import { queryClient } from "@/shared/lib/tanstack/queryClient";
 
 export default function ReactQueryProvider({
   children,
